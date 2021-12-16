88fd6dbacf7849e7e8bbbc093c779a7df4c4811f
novice108 <dshv937@meta.ua>
2021-12-16 12:38:06 +0200

    Create 2021-12-16_TabuOnlySampl.py

---
+++ 2021-12-16_TabuOnlySampl.py
@@ -0,0 +1,62 @@
+from collections import defaultdict
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+from dwave_networkx.utils import binary_quadratic_model_sampler
+import networkx as nx
+import dwave_networkx
+
+# Create empty graph
+G = nx.Graph()
+
+# Add edges to the graph (also adds nodes)
+nbr = 30 #number of vertices
+G=nx.gnp_random_graph(nbr,nbr)
+
+import random
+for u,v,a in G.edges(data=True):
+    G[u][v]['weight'] = 0.1
+
+# ------- Run our QUBO on the QPU -------
+# Set up QPU parameters
+Q= defaultdict(float)
+Q = dwave_networkx.algorithms.tsp.traveling_salesperson_qubo(G, weight='weight')
+#print(Q)
+
+import dimod #binary quadratic model
+bqm = dimod.dimod.BQM.from_qubo(Q)
+#print(bqm)
+
+from dwave.system import LeapHybridSampler
+import numpy as np
+import hybrid
+
+#Start Timer of Problem Computing
+from datetime import datetime
+startDW = datetime.now()
+
+state_start = hybrid.State.from_problem(bqm)
+
+w = hybrid.TabuProblemSampler()
+
+result = w.run(state_start).result()
+#print("Found solution with {} nodes at energy {}.".format(np.sum(result.samples.record.sample),
+#                                                          result.samples.first.energy))
+
+sample = result.samples.first.sample
+#print("result.samples.first.sample ", sample)
+
+route = [None]*len(G)
+for (city, time), val in sample.items():
+    if val:
+        route[time] = city
+
+start = None
+if start is not None and route[0] != start:
+    # rotate to put the start in front
+    idx = route.index(start)
+    route = route[-idx:] + route[:-idx]
+
+#end timer, calculate processing time of problem solving
+print("time of computing TSP ", datetime.now()-startDW)
+
+print("TabuOnly route ", route)
